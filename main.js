d3232ce11d96c7f4f01e32f1b0707fe92700a314
Juan Jose Correa <[200~juanjosecorreamejia@gmail.com~>
2024-07-08 15:03:27 -0500

    se agrego y se arreglo el product detail

--- main.js
+++ main.js
@@ -3,34 +3,34 @@ const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobileIcone = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuCarIcon = document.querySelector('.navbar-shopping-cart');
-const aside = document.querySelector('.product-detail');
+const shoppingCartContainer = document.querySelector('#shoppingCartContaier');
 const cardContainer = document.querySelector('.cards-container');
 
 
 
 menuEmail.addEventListener('click', toggleDesktopMenu);
 menuMobileIcone.addEventListener('click', toggleMobileMenu);
-menuCarIcon.addEventListener('click', toggleCarritoAside)
+menuCarIcon.addEventListener('click', toggleShoppingCartContaier)
 
 function toggleDesktopMenu() 
 {
     mobileMenu.classList.add('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
     desktopMenu.classList.toggle('inactive');
 }
 
 function toggleMobileMenu()
 {
     desktopMenu.classList.add('inactive');
-    aside.classList.add('inactive');
+    shoppingCartContainer.classList.add('inactive');
     mobileMenu.classList.toggle('inactive');
 }
 
-function toggleCarritoAside()
+function toggleShoppingCartContaier()
 {
     desktopMenu.classList.add('inactive');
     mobileMenu.classList.add('inactive');
-    aside.classList.toggle('inactive');
+    shoppingCartContainer.classList.toggle('inactive');
 }
 
 
